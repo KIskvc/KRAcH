2f3bee153248221fbbc4822e22bf04373033d028
Kenan Isakovic <k.isakovic@pke.at>
2025-01-03 20:10:34 +0100

    #6 Implemented Scene Manager, created first view

---
+++ application/blackjack/src/main/java/at/ac/fhcampuswien/blackjack/gui/MainController.java
@@ -0,0 +1,9 @@
+package at.ac.fhcampuswien.blackjack.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class MainController {
+    @FXML
+    private Label welcomeText;
+}
