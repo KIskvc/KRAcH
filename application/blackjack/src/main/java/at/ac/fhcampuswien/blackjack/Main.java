73689b84d086439cf46d81d533cfcd2db9bea5c6
SolimanRana <rana.soliman@stud.fh-campuswien.ac.at>
2025-01-05 21:11:37 +0100

    #1 no necessary changes

--- application/blackjack/src/main/java/at/ac/fhcampuswien/blackjack/Main.java
+++ application/blackjack/src/main/java/at/ac/fhcampuswien/blackjack/Main.java
@@ -1,7 +1,8 @@
 package at.ac.fhcampuswien.blackjack;
+import at.ac.fhcampuswien.blackjack.models.Card;
+import at.ac.fhcampuswien.blackjack.models.Deck;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
     }
 }
