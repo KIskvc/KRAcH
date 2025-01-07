79b20f3e0f5a7ef5df3146b3fa8b9c58a07885b1
harunsefer <harunseferoffice@gmail.com>
2025-01-07 18:20:20 +0100

    leave button implemented with sceneManager

--- application/blackjack/src/main/java/at/ac/fhcampuswien/blackjack/gui/GameController.java
+++ application/blackjack/src/main/java/at/ac/fhcampuswien/blackjack/gui/GameController.java
@@ -5,6 +5,13 @@ import at.ac.fhcampuswien.blackjack.models.Dealer;
 import at.ac.fhcampuswien.blackjack.models.Game;
 import at.ac.fhcampuswien.blackjack.models.Player;
 import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
 
 import java.util.ArrayList;
 
@@ -18,4 +25,9 @@ public class GameController {
         Game game = new Game(player, dealer);
     }
 
+    @FXML
+    public void handleLeaveButton(ActionEvent event) {
+        SceneManager.getInstance().switchScene("main-view.fxml");
+    }
 }
+
